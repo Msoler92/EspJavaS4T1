228497d89b95ca5d95dc75c0b892c765787d529c
Manel <msoler92@gmail.com>
2024-01-07 13:42:13 +0100

    First commit

---
+++ src/main/java/cat/itacademy/barcelonactiva/SoleReina/Manel/s04/t01/n01/S04T01N01SoleReinaManelApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.SoleReina.Manel.s04.t01.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01SoleReinaManelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01SoleReinaManelApplication.class, args);
+	}
+
+}
